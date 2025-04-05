f9b62df925cc1ce9f009b7705b573f09880fa81a
Juan17Informatico <juancampuzano2356@gmail.com>
2025-04-04 19:12:34 -0500

    Cadena nueva

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Hello, World!");
 
 string stripe_api_key="sk_test_4eC39HqLyjWDarjtT1zdp7dc";
+
+string testingConnection = "Server=localhost;Port=5432;User Id=postgres;Password=1234;Database=prueba;Pooling=true;MinPoolSize=0;MaxPoolSize=100;Connection Lifetime=0;Timeout=15;";
